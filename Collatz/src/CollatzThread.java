711c9a579e24e79c10a66a13d4c57d132f774531
StuckBoy <zstuck@nmu.edu>
2017-09-26 20:18:19 -0400

    Threaded stuff runs well?

--- Collatz/src/CollatzThread.java
+++ Collatz/src/CollatzThread.java
@@ -1,22 +1,60 @@
 
 public class CollatzThread {
-	 
-	    public static void main(String[] args) {
-	    	//Track execution time.
-	    	long startTime = System.nanoTime();
-	    	//long[] prev = new long[1000000];
-
+	
+	public static void collatzRecursive(long n) {    	
+    	System.out.print(n + " ");
+    	if (n == 1) return;
+   		else if (n % 2 == 0) collatzRecursive(n / 2);
+   		else collatzRecursive(3*n + 1);
+    } 
+	
+	public static void main(String[] args) {
+	    //Track execution time.
+	    long startTime = System.nanoTime();
+	    Array prev = new Array();	    	
 	    	
-	    	NumCruncher crunchatizer = new NumCruncher();
+	    long theMax = 0;
+	    long theNum = 0;
 	    	
-	    	crunchatizer.start();
-	    	//leCrunch.start();
-	    	//Number crunching time.
-	        long endTime = System.nanoTime();
-	        System.out.println();
-	        System.out.println("Took " + ((double)(endTime - startTime)/1000000000) + " seconds.");
-
+	    NumCruncher crunchatizer = new NumCruncher(1,250000, prev);
+	    NumCruncher leCrunch = new NumCruncher(250001, 500000, prev);
+	    NumCruncher crunchberry = new NumCruncher(500001, 750000, prev);
+	    NumCruncher oops = new NumCruncher(750001, 1000000, prev);
+	    	
+	    crunchatizer.start();
+	    leCrunch.start();
+	    crunchberry.start();
+	    oops.start();
+	    while(true) {
+	    	try {
+	    		crunchatizer.join();
+	    		leCrunch.join();
+	    		crunchberry.join();
+	    		oops.join();
+	    		break;
+	    	} catch (InterruptedException e) {
+	    		e.printStackTrace();
+	    	}
 	    }
+	    
+	    for(long c = 1; c < 1000000; c++) {
+	    	if (prev.getNum(c) > theMax) {
+	    		theMax = prev.getNum(c);
+	    		theNum = c;
+	    	}
+	    	System.out.println("Sequence length for " + c + ": " + prev.getNum(c));
+	    }
+	    
+	   	//Number crunching time.
+	    long endTime = System.nanoTime();
+	    System.out.println();
+	    System.out.println("Took " + ((double)(endTime - startTime)/1000000000) + " seconds.");
+	        
+	    System.out.print("Longest sequence was " + theMax + " from " + theNum + ": ");
+	    //Show the sequence for the longest number less than 1,000,000.
+	    collatzRecursive(theNum);
+			
+	}
 }
 
 class Array { 
@@ -39,54 +77,56 @@ class Array {
 
 class NumCruncher extends Thread {
 	
-	public static void collatzRecursive(long n) {    	
-    	System.out.print(n + " ");
-    	if (n == 1) return;
-   		else if (n % 2 == 0) collatzRecursive(n / 2);
-   		else collatzRecursive(3*n + 1);
-    } 
+	long start;
+	long stop;
+	Array prev;
+	int maxim;
+	long neo;
+	
+	public NumCruncher(long a, long b, Array s) {
+		start = a;
+		stop = b;
+		prev = s;
+	}
 	
 	public synchronized void run() {
-		Array prev = new Array();
-    	int maxCount = 0;
-    	long whichNum = 0;
-    	for (long i = 1; i < 1000000; i++) {
+	//	long startTime = System.nanoTime();
+    	for (long i = start; i < stop; i++) {
     		int count = 0;
     		boolean tooBig = false;
         	long startNum = i;
         	long n = startNum;
         	//While the sequence hasn't resolved.
-        	while (n != 1) {
-        		if (n >= 1000000) tooBig = true;
-        		else tooBig = false;
+        	synchronized(prev) {
+        		while (n != 1) {
+        			if (n >= 1000000) tooBig = true;
+        			else tooBig = false;
         		//If the number is too big to check or there isn't a sequence for it.
-        		if (tooBig || prev.checkNum(n)) {
-             		if (n % 2 == 0) n = n / 2;
-        			else n = (3*n) + 1;
-        			count++;
-        		}
+        			if (tooBig || prev.checkNum(n)) {
+             			if (n % 2 == 0) n = n / 2;
+             			else n = (3*n) + 1;
+             			count++;
+        			}
         		//There's already a sequence generated for this number.
-        		else {
+        			else {
         			//System.out.print("Existing entry found!");
-        			count += prev.getNum(n);
-        			break;
+        				count += prev.getNum(n);
+        				break;
+        			}
         		}
-        	}
         	//Add the sequence length for the number just processed.
-        	prev.setNum(i, count);
-        	
-        	System.out.print("Sequence length for " + startNum + ": " + count);
-        	System.out.println();
-        	
-        	//If the sequence was longer than the current maxCount, replace with count.
-        	if (count > maxCount) {
-        		maxCount = count;
-        		whichNum = startNum;
+        		prev.setNum(i, count);
         	}
-        } 
-    	System.out.print("Longest sequence was " + maxCount + " from " + whichNum + ": ");
-    	//Show the sequence for the longest number less than 1,000,000.
-    	collatzRecursive(whichNum);
-		
+//        	System.out.print("Sequence length for " + startNum + ": " + count);
+ //       	System.out.println();
+        	//If the sequence was longer than the current maxCount, replace with count.
+        	if (count > maxim) {
+        		maxim = count;
+        		neo = startNum;
+        	}
+        }
+  //      long endTime = System.nanoTime();
+ //       System.out.println();
+//        System.out.println("Took " + ((double)(endTime - startTime)/1000000000) + " seconds.");
 	}
 }
